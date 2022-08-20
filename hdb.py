8b98cd703018eeb26071524ee6a7868d3537f547
Elias Ngige <elias.ngige@burnmfg.com>
2022-08-20 12:31:34 +0300

    Happiest birthday

---
+++ hdb.py
@@ -0,0 +1,20 @@
+#Happy Birthday Pythonester
+#Authoured By @Elii254
+import time 
+
+
+def happy_birthday(name):
+    name = name.capitalize()
+    for x in range(2):
+       
+        print("Happy Birthday ")
+        time.sleep(1)
+
+print("Happy Birthday to you. Poleen")
+time.sleep(1)
+
+for x in range(3):
+    print("You were born to be a winner!")
+
+
+happy_birthday("Happy Birthday Poleen")
